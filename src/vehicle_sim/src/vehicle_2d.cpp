85936fb6fddd083ba116304fcf8b623fc003cd2d
Tanmay <tanmaychhatbar@gmail.com>
2025-04-17 22:49:01 -0700

    add calculator functions for 2D vehicle_sim

--- src/vehicle_sim/src/vehicle_2d.cpp
+++ src/vehicle_sim/src/vehicle_2d.cpp
@@ -13,6 +13,12 @@ public:
           vx(0.0), vy(0.0), w_yaw(0.0),
           v_wheel{0.0, 0.0, 0.0, 0.0} {}
 
+    // getters and setters for inputs
+    double getSteeringAngle() const { return steering_angle; }
+    void setSteeringAngle(double steering_angle_) { steering_angle = steering_angle_; }
+    double getThrottle() const { return throttle; }
+    void setThrottle(double throttle_) { throttle = throttle_; }
+
     // getters and setters for position and orientation
     void getPosition(double &x_, double &y_) const
     {
@@ -40,15 +46,20 @@ public:
     }
     void setAngularVelocities(double w_yaw_) { w_yaw = w_yaw_; }
     void getAngularVelocities(double &w_yaw_) const { w_yaw_ = w_yaw; }
+
     void getWheelVelocities(double &v_fl_, double &v_fr_, double &v_rl_, double &v_rr_)
     {
-        v_fl_ = v_wheel[0]; v_fr_ = v_wheel[1];
-        v_rl_ = v_wheel[2]; v_rr_ = v_wheel[3];
+        v_fl_ = v_wheel[0];
+        v_fr_ = v_wheel[1];
+        v_rl_ = v_wheel[2];
+        v_rr_ = v_wheel[3];
     }
     void setWheelVelocities(double v_fl_, double v_fr_, double v_rl_, double v_rr_)
     {
-        v_wheel[0] = v_fl_; v_wheel[1] = v_fr_;
-        v_wheel[2] = v_rl_; v_wheel[3] = v_rr_;
+        v_wheel[0] = v_fl_;
+        v_wheel[1] = v_fr_;
+        v_wheel[2] = v_rl_;
+        v_wheel[3] = v_rr_;
     }
     void getWheelVelocities(double (&v_wheel_)[4]) const
     {
@@ -80,13 +91,17 @@ public:
     void setAngularAccelerations(double a_yaw_) { a_yaw = a_yaw_; }
     void getWheelAccelerations(double &a_fl_, double &a_fr_, double &a_rl_, double &a_rr_)
     {
-        a_fl_ = a_wheel[0]; a_fr_ = a_wheel[1];
-        a_rl_ = a_wheel[2]; a_rr_ = a_wheel[3];
+        a_fl_ = a_wheel[0];
+        a_fr_ = a_wheel[1];
+        a_rl_ = a_wheel[2];
+        a_rr_ = a_wheel[3];
     }
     void setWheelAccelerations(double a_fl_, double a_fr_, double a_rl_, double a_rr_)
     {
-        a_wheel[0] = a_fl_; a_wheel[1] = a_fr_;
-        a_wheel[2] = a_rl_; a_wheel[3] = a_rr_;
+        a_wheel[0] = a_fl_;
+        a_wheel[1] = a_fr_;
+        a_wheel[2] = a_rl_;
+        a_wheel[3] = a_rr_;
     }
     void getWheelAccelerations(double (&a_wheel_)[4]) const
     {
@@ -103,6 +118,47 @@ public:
         }
     }
 
+    // getters and setters for forces and moments
+    void getBodyForcesAndMoments(double &Fx_, double &Fy_, double &Mz_) const
+    {
+        Fx_ = Fx;
+        Fy_ = Fy;
+        Mz_ = Mz;
+    }
+    void setBodyForcesAndMoments(double Fx_, double Fy_, double Mz_)
+    {
+        Fx = Fx_;
+        Fy = Fy_;
+        Mz = Mz_;
+    }
+    void getWheelNormalLoads(double &Fz_fl_, double &Fz_fr_, double &Fz_rl_, double &Fz_rr_) const
+    {
+        Fz_fl_ = Fz_wheel[0];
+        Fz_fr_ = Fz_wheel[1];
+        Fz_rl_ = Fz_wheel[2];
+        Fz_rr_ = Fz_wheel[3];
+    }
+    void setWheelNormalLoads(double Fz_fl_, double Fz_fr_, double Fz_rl_, double Fz_rr_)
+    {
+        Fx_wheel[0] = Fz_fl_;
+        Fx_wheel[1] = Fz_fr_;
+        Fx_wheel[2] = Fz_rl_;
+        Fx_wheel[3] = Fz_rr_;
+    }
+    void getWheelNormalLoads(double (&Fz_wheel_)[4]) const
+    {
+        Fz_wheel_[0] = Fz_wheel[0];
+        Fz_wheel_[1] = Fz_wheel[1];
+        Fz_wheel_[2] = Fz_wheel[2];
+        Fz_wheel_[3] = Fz_wheel[3];
+    }
+    void setWheelNormalLoads(const double Fz_wheel_[4])
+    {
+        Fx_wheel[0] = Fz_wheel_[0];
+        Fx_wheel[1] = Fz_wheel_[1];
+        Fx_wheel[2] = Fz_wheel_[2];
+        Fx_wheel[3] = Fz_wheel_[3];
+    }
 private:
     // global 2D position
     double X, Y;
@@ -128,6 +184,10 @@ private:
     double Fy_wheel[4];
     double Fz_wheel[4];
 
+    // inputs
+    double steering_angle;
+    double throttle;
+
     friend class Vehicle;
 };
 
@@ -142,33 +202,47 @@ public:
         : B(B), C(C), D(D), E(E) {}
 
     // tire forces
-    double calculateLateralForce(double slipAngle) const
-    {
-        return D * std::sin(C * std::atan(B * slipAngle - E * (B * slipAngle - std::atan(B * slipAngle))));
-    }
-
-    double calculateLongitudinalForce(double slipRatio) const
-    {
-        return D * std::sin(C * std::atan(B * slipRatio - E * (B * slipRatio - std::atan(B * slipRatio))));
-    }
-
-    double calcCombinedSlipForce(double slipAngle, double slipRatio) const
-    {
-        return 0.0;
-    }
-    double setTireConfig(double B_, double C_, double D_, double E_)
+    double setTireConfig(double B_, double C_, double D_, double E_, double f_)
     {
         B = B_;
         C = C_;
         D = D_;
         E = E_;
+        f = f_;
+    }
+    double getTireConfig(double &B_, double &C_, double &D_, double &E_, double &f_) const
+    {
+        B_ = B;
+        C_ = C;
+        D_ = D;
+        E_ = E;
+        f_ = f;
+    }
+
+    void calcTireForces(double slipAngle, double slipRatio, double Fz_wheel, double &Fx_wheel, double &Fy_wheel)
+    {
+        // calculate tire forces using pacejka model
+        double slipNet = sqrt(slipAngle * slipAngle + slipRatio * slipRatio * f * f);
+
+        // pacejka tire formula
+        if (slipNet == 0.0)
+        {
+            Fx_wheel = 0.0;
+            Fy_wheel = 0.0;
+            return;
+        }
+        double Fnet = D * sin(C * atan(B * slipNet - E * (B * slipNet - atan(B * slipNet))));
+        Fx_wheel = Fnet * slipRatio * f / slipNet;
+        Fy_wheel = Fnet * slipAngle / slipNet;
     }
 
 private:
-    double B;                   // stiffness factor
-    double C;                   // shape factor
-    double D;                   // peak factor
-    double E;                   // curvature factor
+    double B; // stiffness factor
+    double C; // shape factor
+    double D; // peak factor
+    double E; // curvature factor
+    double f; // coeff of relative stiffness for long and lateral directions
+
     friend class VehicleConfig; // allow VehicleConfig to access private members
     friend class Vehicle;       // allow Vehicle to access private members
 };
@@ -186,6 +260,15 @@ public:
         : wheelbase(wheelbase_), track_width(track_width_), steer_max(steer_max_),
           mass(mass_), Izz(Izz_), z_cg(z_cg_), a(a_), tire_config(tire_config_) {}
 
+    double getMass() const { return mass; }
+    double getIzz() const { return Izz; }
+    double getWheelbase() const { return wheelbase; }
+    double getTrackWidth() const { return track_width; }
+    double getSteerMax() const { return steer_max; }
+    double getZcg() const { return z_cg; }
+    double getA() const { return a; }
+    double getWheelRadius() const { return r_wheel; }
+
 private:
     double wheelbase;   // [m] distance between front and rear axles
     double track_width; // [m] distance between left and right wheels
@@ -194,6 +277,8 @@ private:
     double Izz;         // [kgm2] moment of inertia about the z-axis
     double z_cg;        // [m] static center of gravity height from ground
     double a;           // [m] distance from front axle to center of gravity
+    double r_wheel;
+    double Tmax;
 
     TireConfig tire_config; // tire parameters
 
@@ -206,42 +291,163 @@ public:
     Vehicle(const VehicleData &data_, const VehicleConfig &config_)
         : data(data_), config(config_) {}
 
-    void calcTireForces(double &Fx, double &Fy)
+    void calcWheelSlipsAndForces()
     {
-        // Calculate forward slip velocity
 
-        // Calculate forward force
+        // get vehicle states
+        double vx, vy, w_yaw;
+        data.getLinearVelocities(vx, vy);
+        data.setAngularVelocities(w_yaw);
+        double steering_angle = data.getSteeringAngle();
+        double a = config.getA();
+        double b = config.getWheelbase() - a;
+        double half_track_width = config.getTrackWidth() / 2.0;
+        double r_wheel = config.getWheelRadius();
+        double v_wheel[4];
+        data.getWheelVelocities(v_wheel);
 
-        // Calculate lateral slip velocity
+        double slipAngle[4];
+        double slipRatio[4];
+        double vxw[4] = {
+            vx - half_track_width * w_yaw,
+            vx + half_track_width * w_yaw,
+            vx - half_track_width * w_yaw,
+            vx + half_track_width * w_yaw};
+        double vyw[4] = {
+            vy + a * w_yaw,
+            vy + a * w_yaw,
+            vy - b * w_yaw,
+            vy - b * w_yaw};
+        slipAngle[0] = atan2(vyw[0], vxw[0]) - steering_angle; // front left
+        slipAngle[1] = atan2(vyw[1], vxw[1]) - steering_angle; // front right
+        slipAngle[2] = atan2(vyw[2], vxw[2]);                  // rear left
+        slipAngle[3] = atan2(vyw[3], vxw[3]);                  // rear right
 
-        // Calculate lateral force
+        double Fx_wheel[4];
+        double Fy_wheel[4];
+        for (int i = 0; i < 4; ++i)
+        {
+            // calc net slip
+            slipRatio[i] = v_wheel[i] * r_wheel / vxw[i] - 1; // front left
+            double slipNet = sqrt(slipAngle[i] * slipAngle[i] + slipRatio[i] * slipRatio[i]);
+
+            // calculate tire forces
+            config.tire_config.calcTireForces(slipAngle[i], slipRatio[i], data.Fz_wheel[i], Fx_wheel[i], Fy_wheel[i]);
+        }
     }
-    void calcLinearAccelerations();
-    void calcAngularAccelerations();
-    void stepSimulation(double dt)
+    void calcTireNormalLoads() // update Fz_wheel array
     {
-        // Update vehicle state based on the current input and dt
+        // get vehicle parameters
+        double m = config.getMass();
+        double g = 9.81; // gravity
+        double a = config.getA();
+        double b = config.getWheelbase() - a;
+        double h = config.getZcg();
+        double track_width = config.getTrackWidth();
+        double wheelbase = config.getWheelbase();
+
+        double Fz_front = m * g * b / (a + b) / 2.0;
+        double Fz_rear = m * g * a / (a + b) / 2.0;
+
+        // calc load transfer
+        double dFz_x = h * m * data.ax / config.wheelbase;
+        double dFz_y = h * m * data.ay / (config.track_width / 2.0);
+
+        // set normal loads on each wheel
+        double Fz_wheel[4] = {
+            Fz_front - dFz_x / 2.0 - dFz_y / 2.0, // front left
+            Fz_front - dFz_x / 2.0 + dFz_y / 2.0, // front right
+            Fz_rear + dFz_x / 2.0 - dFz_y / 2.0,  // rear left
+            Fz_rear + dFz_x / 2.0 + dFz_y / 2.0   // rear right
+        };
+        data.setWheelNormalLoads(Fz_wheel);
+    }
+    void calcNetForcesAndMoments() // update Fx, Fy, Mz
+    {
+        // calculate net forces and moments acting on the vehicle
+        double Fx = 0.0;
+        double Fy = 0.0;
+        double Mz = 0.0;
+        for (int i = 0; i < 2; i++)
+        {
+            Fx += data.Fx_wheel[i] * cos(data.steering_angle) - data.Fy_wheel[i] * sin(data.steering_angle);
+            Fy += data.Fy_wheel[i] * sin(data.steering_angle) + data.Fx_wheel[i] * cos(data.steering_angle);
+        }
+        for (int i = 2; i < 4; i++)
+        {
+            Fx += data.Fx_wheel[i];
+            Fy += data.Fy_wheel[i];
+        }
+        Mz += -data.Fx_wheel[0] * config.track_width / 2.0 + data.Fy_wheel[0] * config.a;
+        Mz += -data.Fx_wheel[1] * config.track_width / 2.0 + data.Fy_wheel[1] * config.a;
+        Mz += -data.Fx_wheel[2] * config.track_width / 2.0 - data.Fy_wheel[2] * config.a;
+        Mz += -data.Fx_wheel[3] * config.track_width / 2.0 - data.Fy_wheel[3] * config.a;
+        data.setBodyForcesAndMoments(Fx, Fy, Mz);
+    }
+    void calcAccelerations() // update resultant body accelerations ax, ay, a_yaw
+    {
+        double ax, ay, a_yaw, Fx, Fy, Mz;
+        double mass = config.getMass();
+        double Izz = config.getIzz();
+        data.getBodyForcesAndMoments(Fx, Fy, Mz);
+        data.getLinearAccelerations(ax, ay);
+        data.getAngularAccelerations(a_yaw);
+
+        // calculate resultant body accelerations based on net forces and moments
+        ax = Fx / config.mass;
+        ay = Fy / config.mass;
+        a_yaw = Mz / config.Izz;
+
+        data.setLinearAccelerations(ax, ay);
+        data.setAngularAccelerations(a_yaw);
+    }
+    void calcNewState(double dt) // update vehicle state (X, Y, yaw, vx, vy, w_yaw)
+    {
+        double vx, vy, w_yaw;
+        data.getLinearVelocities(vx, vy);
+        data.getAngularVelocities(w_yaw);
+
         double ax, ay, a_yaw;
-        calcLinearAccelerations();
-        calcAngularAccelerations();
+        data.getLinearAccelerations(ax, ay);
+        data.getAngularAccelerations(a_yaw);
 
-        // calculate accelerations without centrifugal terms
-        data.vx += (ax - data.w_yaw * data.vy) * dt;
-        data.vy += (ay + data.w_yaw * data.vx) * dt;
-        data.w_yaw += a_yaw * dt;
+        double X, Y, yaw;
+        data.getPosition(X, Y);
+        data.getOrientation(yaw);
 
-        // update global position
-        data.X += (data.vx * std::cos(data.yaw) - data.vy * std::sin(data.yaw)) * dt;
-        data.Y += (data.vx * std::sin(data.yaw) + data.vy * std::cos(data.yaw)) * dt;
+        vx += (ax - w_yaw * vy) * dt;
+        vy += (ay + w_yaw * vx) * dt;
+        w_yaw += a_yaw * dt;
 
-        // TODO update global position including acceleration terms
-        // data.X += (data.vx * std::cos(data.yaw) - data.vy * std::sin(data.yaw)) * dt +
-        //      0.5 * (ax * std::cos(data.yaw) - ay * std::sin(data.yaw)) * dt * dt;
-        // data.Y += (data.vx * std::sin(data.yaw) + data.vy * std::cos(data.yaw)) * dt +
-        //      0.5 * (ax * std::sin(data.yaw) + ay * std::cos(data.yaw)) * dt * dt;
-        data.yaw += data.w_yaw * dt + 0.5 * data.a_yaw * dt * dt;
+        X += (vx * std::cos(yaw) - vy * std::sin(yaw)) * dt;
+        Y += (vx * std::sin(yaw) + vy * std::cos(yaw)) * dt;
+        yaw += w_yaw * dt + 0.5 * a_yaw * dt * dt;
+
+        double v_wheel[4];
+        double a_wheel[4];
+        data.getWheelVelocities(v_wheel);
+        data.getWheelAccelerations(a_wheel);
+        for (int i = 0; i < 4; ++i)
+        {
+            v_wheel[i] += a_wheel[i] * dt;
+        }
+        data.setWheelVelocities(v_wheel);
     }
-
+    void stepSimulation() 
+    {
+        setInput(0, 1);
+        calcWheelSlipsAndForces();
+        calcTireNormalLoads();
+        calcNetForcesAndMoments();
+        calcAccelerations();
+        double dt = 0.01; // time step
+        calcNewState(dt);
+    }
+    void setInput(double steering_angle, double throttle)
+    {
+        data.setSteeringAngle(steering_angle);
+        data.setThrottle(throttle);
+    }
 private:
     VehicleData data;
     VehicleConfig config;
--- src/vehicle_sim/src/vehicle_2d.hpp
+++ src/vehicle_sim/src/vehicle_2d.hpp
@@ -3,6 +3,26 @@
 #include <cmath>    // for mathematical constants if needed
 #include <iostream> // for printing
 
+
+class VehicleInput
+{
+    public:
+        // constructor
+        VehicleInput()
+            : steering_angle(0.0), motor_torque(0.0) {}
+
+        // getters and setters for steering angle and motor torque
+        void getSteeringAngle(double &steering_angle_) const;
+        void setSteeringAngle(double steering_angle_);
+        void getMotorTorque(double &motor_torque_) const;
+        void setMotorTorque(double motor_torque_);
+    private:
+        double steering_angle; // [rad] steering angle
+        double motor_torque;   // [Nm] motor torque
+
+        friend class Vehicle; // allow Vehicle to access private members
+};
+
 // vehicle state data
 class VehicleData
 {
@@ -24,6 +44,7 @@ public:
     void setLinearVelocities(double vx_, double vy_);
     void getAngularVelocities(double &w_yaw_) const;
     void setAngularVelocities(double w_yaw_);
+
     void getWheelVelocities(double &fl_, double &fr_, double &rl_, double &rr_) const;
     void setWheelVelocities(double fl_, double fr_, double rl_, double rr_);
     void getWheelVelocities(double (&v_wheel_)[4]) const;
@@ -38,6 +59,15 @@ public:
     void setWheelAccelerations(double a_fl_, double a_fr_, double a_rl_, double a_rr_);
     void getWheelAccelerations(double (&v_wheel_)[4]) const;
     void setWheelAccelerations(const double v_wheel_[4]);
+
+    // getters and setters for forces and moments
+    void getBodyForcesAndMoments(double &Fx_, double &Fy_, double &Mz_) const;
+    void getBodyForcesAndMoments(double Fx_, double Fy_, double Mz_);
+    void getWheelNormalLoads(double &Fz_fl_, double &Fz_fr_, double &Fz_rl_, double &Fz_rr_) const;
+    void setWheelNormalLoads(double Fz_fl_, double Fz_fr_, double Fz_rl_, double Fz_rr_);
+    void getWheelNormalLoads(double (&Fz_wheel_)[4]) const;
+    void setWheelNormalLoads(const double Fz_wheel_[4]);
+
 
 private:
     // global 2D position
@@ -64,6 +94,10 @@ private:
     double Fy_wheel[4];
     double Fz_wheel[4];
 
+    // inputs
+    double steering_angle;
+    double throttle;
+
     friend class Vehicle;
 };
 
@@ -73,21 +107,22 @@ class TireConfig
 public:
     // constructor
     TireConfig()
-        : B(0.0), C(0.0), D(0.0), E(0.0) {}
-    TireConfig(double B, double C, double D, double E)
-        : B(B), C(C), D(D), E(E) {}
+        : B(0.12), C(0.0), D(0.9), E(0.0), f(1.0) {}
+    TireConfig(double B, double C, double D, double E, double f)
+        : B(B), C(C), D(D), E(E), f(f) {}
 
     // tire forces
-    double calculateLateralForce(double slipAngle);
-    double calculateLongitudinalForce(double slipRatio);
-    double calcCombinedSlipForce(double slipAngle, double slipRatio);
     double setTireConfig(double B, double C, double D, double E);
+    double getTireConfig(double &B_, double &C_, double &D_, double &E_) const;
 
+    void calcTireForces(double slipAngle, double slipRatio, double Fz_wheel, double &Fx_wheel, double &Fy_wheel);
 private:
     double B; // stiffness factor
     double C; // shape factor
     double D; // peak factor
     double E; // curvature factor
+    double f; // coeff of relative stiffness for long and lateral directions
+
     friend class VehicleConfig; // allow VehicleConfig to access private members
     friend class Vehicle;      // allow Vehicle to access private members
 };
@@ -104,7 +139,16 @@ public:
                   double z_cg_, double a_, TireConfig tire_config_)
         : wheelbase(wheelbase_), track_width(track_width_), steer_max(steer_max_),
           mass(mass_), Izz(Izz_), z_cg(z_cg_), a(a_), tire_config(tire_config_) {}
-
+    
+    // getters for vehicle parameters
+    double getMass() const;
+    double getIzz() const;
+    double getWheelbase() const;
+    double getTrackWidth() const;
+    double getSteerMax() const;
+    double getZcg() const;
+    double getA() const;
+    double getWheelRadius() const { return r_wheel; }
 private:
     double wheelbase;   // [m] distance between front and rear axles
     double track_width; // [m] distance between left and right wheels
@@ -113,7 +157,8 @@ private:
     double Izz;         // [kgm2] moment of inertia about the z-axis
     double z_cg;        // [m] static center of gravity height from ground
     double a;           // [m] distance from front axle to center of gravity
-
+    double r_wheel; // [m] wheel radius
+    double Tmax; // [Nm] maximum torque
     TireConfig tire_config; // tire parameters
 
     friend class Vehicle;
@@ -122,14 +167,20 @@ private:
 class Vehicle
 {
 public:
-    Vehicle(const VehicleData &data_, const VehicleConfig &config_)
+    Vehicle(const VehicleData &data_, const VehicleConfig &config_, const VehicleInput &input_)
         : data(data_), config(config_) {}
+    Vehicle(const VehicleConfig &config_)
+        : data(VehicleData()), config(config_) {}
 
-    void calcTireForces(double &Fx, double &Fy);
-    void calcLinearAccelerations();
-    void calcAngularAccelerations();
-
+    void calcWheelSlipsAndForces();
+    void calcTireNormalLoads(); // update Fz_wheel array
+    void calcNetForcesAndMoments();       // update Fx, Fy, Mz
+    void calcAccelerations();   // update resultant body accelerations ax, ay, a_yaw
+    void calcNewState(double dt);         // update vehicle state (X, Y, yaw, vx, vy, w_yaw)
+    void stepSimulation();
+    void setInput(double steering_angle, double throttle);
 private:
     VehicleData data;
     VehicleConfig config;
+    VehicleInput input;
 };
