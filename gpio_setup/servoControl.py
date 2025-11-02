72df10e1e78c3f58290bfdf20d74562c2c3ab231
Tanmay Chhatbar <tanmaychhatbar@gmail.com>
2025-11-02 14:31:03 -0800

    joystick to PCA9685 test

--- gpio_setup/servoControl.py
+++ gpio_setup/servoControl.py
@@ -1,9 +1,33 @@
 import board
 from adafruit_pca9685 import PCA9685
 
+import pygame
+from racecar_joystick import RacecarJoystick
+
 print(f"i2c pins:\n\tSDA={board.SDA}\n\tSCL={board.SCL}")
 i2c = board.I2C()  # uses board.SCL and board.SDA
 pca = PCA9685(i2c)
 pca.frequency = 1e2
 
-pca.channels[0].duty_cycle = 0x7FFF
+steering = servo.Servo(pca.channels[7])
+throttle = servo.Servo(pca.channels[6])
+
+joystick = RacecarJoystick()
+
+def main():
+    while True:
+        joystick.update()
+        if joystick.isNewInput():
+            print(f"{' ' if joystick.steering >= 0 else ''}{joystick.steering:.2f}, {joystick.throttle:.2f}, {joystick.brake:.2f}")
+            steering.value = joystick.steering
+            throttle.value = joystick.throttle - joystick.brake
+
+if __name__ == "__main__":
+    try:
+        main()
+    except KeyboardInterrupt:
+        print('exiting')
+        steering.value = 0
+        throttle.value = 0
+        pca.deinit()
+        pygame.quit()
