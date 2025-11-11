ea49ad773f2987ae8be817ef32937178da4f4245
Tanmay Chhatbar <tanmaychhatbar@gmail.com>
2025-11-10 20:17:42 -0800

    add camera to telemetry

--- src/telemetry/launch/telemetry_launch.py
+++ src/telemetry/launch/telemetry_launch.py
@@ -9,5 +9,17 @@ def generate_launch_description():
         executable='icm20948_ros'
     )
 
+    camera_telemetry_node = Node(
+        package='camera_ros',
+        executable='camera_node',
+        parameters=[{
+                'orientation': 180,
+                'width': 160,
+                'height': 120,
+                'camera': '/base/axi/pcie@120000/rp1/i2c@88000/imx708@1a'
+            }]
+    )
+
     ld.add_action(imu_telemetry_node)
+    ld.add_action(camera_telemetry_node)
     return ld
