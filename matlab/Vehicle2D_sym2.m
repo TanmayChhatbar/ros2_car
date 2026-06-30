ae972001119970938c8021edeb5774e46c8e90a0
Tanmay Chhatbar <tanmaychhatbar@gmail.com>
2026-06-30 13:27:14 -0400

    basic global search

--- matlab/Vehicle2D_sym2.m
+++ matlab/Vehicle2D_sym2.m
@@ -33,31 +33,51 @@ vehicle.data.vx = sym(1);
 %                     "opt_steering_angle [rad]",
 %                     "opt_yawrate [rad/s]"});
 x_des = [1, 0];
-u0 = [1, pi/4, 0];
-lb = [0 -pi/2 0.1];
-ub = [1e3 pi/2 5];
+u0 = [1, pi/4, 0]; % wheel speed, steering angle, yaw rate
+lb = [0 -2*pi/3  -5];
+ub = [1e3 2*pi/3 5];
 
 % optimize u to make all values in g = 0
-f = @(u) getStateGradient(vehicle, x_des, u)
+getStateGradient(vehicle, x_des, u0)
+fv = @(u) getStateGradient(vehicle, x_des, u);
+f = @(u) getStateGradient(vehicle, x_des, u, true);
 
 % fmincon
 % options = optimoptions('fmincon', 'Display','iter');
 % uopt = fmincon(f, u0, [], [], [], [], lb, ub, [], options)
-% [~, gv] = getStateGradient(vehicle, x_des, uopt)
+% gv = getStateGradient(vehicle, x_des, uopt)
+
+% globalsearch
+opts = optimoptions(@fmincon, 'Algorithm','sqp', 'Display', 'iter');
+problem = createOptimProblem('fmincon','objective',...
+    f,'x0',u0,'lb',lb,'ub',ub,'options',opts);
+gs = GlobalSearch;
+[uopt,f] = run(gs,problem)
+gv = getStateGradient(vehicle, x_des, uopt)
+
 
 % fgoalattain
-options = optimoptions('fgoalattain', 'Display','iter', ...
-    'StepTolerance', 1e-20, ...
-    'FunctionTolerance', 1e-20, ...
-    'OptimalityTolerance', 1e-34)
-[uopt, gv] = fgoalattain(f, u0, zeros(1, 5), ones(1, 5), ...
-    [], [], [], [], [], [], [], options)
+% options = optimoptions('fgoalattain', 'Display','iter', ...
+%     'StepTolerance', 1e-20, ...
+%     'FunctionTolerance', 1e-20, ...
+%     'OptimalityTolerance', 1e-34)
+% [uopt, gv] = fgoalattain(fv, u0, zeros(1, 5), ones(1, 5), ...
+%     [], [], [], [], lb, ub, [], options)
+
+% opts = optimoptions(@fmincon, 'UseParallel', true);
+% problem = createOptimProblem('fmincon','objective',...
+%     f,'x0',u0,'lb',lb,'ub',ub);
+% ms = MultiStart;
+% [uopt,f] = run(ms,problem, 200)
 % gv = getStateGradient(vehicle, x_des, uopt)
 
 % result
 %% functions
-% function [g, gv] = getStateGradient(vehicle, x, u)
-function gv = getStateGradient(vehicle, x, u)
+function gv = getStateGradient(vehicle, x, u, scalar_out)
+if nargin < 4
+    scalar_out = false;
+end
+
 vehicle.data.vx = x(1);
 vehicle.data.vy = x(2);
 
@@ -65,31 +85,32 @@ w_rear_wheels = u(1);
 steering_angle = u(2);
 yaw_rate = u(3);
 
-% vehicle.input.throttle_input = throttle_input;
-% vehicle.input.brake_input = brake_input;
+% zero vehicle inputs
 vehicle.input.throttle_input = 0;
 vehicle.input.brake_input = 0;
-% vehicle.calcSteeringAngle(); % apply steering_input
+
+% apply optimization-control inputs
 vehicle.data.steering_angle = steering_angle;
-vehicle.data.w_wheel(3:4) = w_rear_wheels; % apply w_rear_wheels
-vehicle.data.w_yaw = yaw_rate; % apply yaw rate
-% vehicle.calcMotorTorque();
-% vehicle.calcBrakeTorque();
-% vehicle.calcTractionTorques();
+vehicle.data.w_wheel(3:4) = w_rear_wheels;
+vehicle.data.w_yaw = yaw_rate;
+
+% wheel torques = 0
 vehicle.data.motor_torque = 0;
 vehicle.data.brake_torque = zeros(1, 4);
 vehicle.data.wheel_torques = zeros(1, 4);
-vehicle.calcTireNormalLoads();
-w_front_wheels_prev = [-inf -inf];
+a_front_wheels_prev = [-inf -inf];
+vehicle.data.a_wheel(1:2) = inf;
 n = 0;
 dtt = 0.001;
-while any(abs(w_front_wheels_prev - vehicle.data.w_wheel(1:2)) > 1e-3)
-    if n > 200
-        dtt = 0.00001;
-    elseif n > 20
-        dtt = 0.0001;
+while any(abs(vehicle.data.a_wheel(1:2)) > 1) && n < 1e4
+    if any(sign(a_front_wheels_prev) .* sign(vehicle.data.a_wheel(1:2)) == -1)
+        dtt = dtt / 10;
+        if dtt < 0.000001
+            break
+        end
     end
-    w_front_wheels_prev = vehicle.data.w_wheel(1:2);
+    a_front_wheels_prev = vehicle.data.a_wheel(1:2);
+    vehicle.calcTireNormalLoads();
     vehicle.calcWheelSlipsAndForces();
     vehicle.calcWheelAccelerations();
     vehicle.data.w_wheel(1:2) = ...
@@ -110,6 +131,8 @@ gv = double([(ax + w_yaw * vy); % d(vx) / dt
     a_yaw;              % d(w_yaw) / dt
     vehicle.data.a_wheel(1:2)'; ... % change of front wheel speeds
     ]);
-% disp(gv')
-g = sum(gv.^2);
+
+if scalar_out
+    gv = sum(gv.^2);
+end
 end
