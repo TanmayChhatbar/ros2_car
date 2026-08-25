7043721c01a2ba94f6db3c46327b46275e7bad5e
Tanmay Chhatbar <tanmaychhatbar@gmail.com>
2026-08-25 17:15:58 -0400

    minor changes

--- matlab/Vehicle2D_map_sym2.m
+++ matlab/Vehicle2D_map_sym2.m
@@ -79,11 +79,11 @@ v2.data = subs2(v2.data, string(v2.data.p_wheel), 0);
 v2.data = subs2(v2.data, string(v2.data.wheel_torques), 0);
 
 % simplifications
+% ax = -w*vy
+% ay = w*vx
 v2.data = subs2(v2.data, "ax", -v2.data.w_yaw * v2.data.vy);
 v2.data = subs2(v2.data, "ay", v2.data.w_yaw * v2.data.vx);
 
-% ax = -w*vy
-% ay = w*vx
 % w = -ax/vy = ay/vx
 % ay = -ax*vx/vy
 % v2.data = subs2(v2.data, "w_yaw", -v2.data.ax / v2.data.vy);
@@ -99,8 +99,6 @@ v2.data = subs2(v2.data, "ay", v2.data.w_yaw * v2.data.vx);
 % d(w_wheel1)/dt = a_wheel1 = 0
 % d(w_wheel2)/dt = a_wheel2 = 0
 
-% vx_target = 1;
-% vy_target = 0;
 syms vx_target vy_target
 
 syms w_wheel_rear
@@ -118,42 +116,41 @@ eqn(2) = v3.data.a_wheel(1) == 0;
 eqn(3) = v3.data.a_wheel(2) == 0;
 eqn(4) = sum(v3.data.Fx_wheel) - v3.config.mass * (-v3.data.w_yaw * v3.data.vy) == 0;
 eqn(5) = sum(v3.data.Fy_wheel) - v3.config.mass * ( v3.data.w_yaw * v3.data.vx) == 0;
-% eqn = simplify(expand(eqn));
 sv = symvar(eqn);
-idx_targets = string(sv) == "vx_target" | string(sv) == "vy_target";
+idx_targets = contains(string(sv), "_target");
 uv = sv(idx_targets)
 sv = sv(~idx_targets)
 
-%% solve using optimization
+return
+%% solve
 t_vx = 1;
-t_vy = 0;
+t_vy = -0.05;
 
 % sub targets
 eqn_vsub = subs(eqn, 'vx_target', t_vx);
 eqn_vsub = subs(eqn_vsub, 'vy_target', t_vy);
-wz = 0;
-delta = 0;
-w_front = solveFrontWheelSpeeds(t_vx, t_vy, wz, delta, v3.config.r_wheel, eqn_vsub(2:3));
+% eqn_vsub = simplify(lhs(eqn_vsub)) == 0;
 
-eqn_sub = subs(eqn_vsub, 'w_wheel1', w_front(1));
-eqn_sub = subs(eqn_sub, 'w_wheel2', w_front(2));
+% estimate initial stuff
+est_beta = atan2(t_vy, t_vx);
+est_v = norm([t_vy, t_vx]);
+assumption_latacc = 0.8*9.81 * est_beta;
+est_wz = assumption_latacc / est_v;
+vy_f(1) = t_vy + est_wz * v3.config.a;
+vy_f(2) = t_vy + est_wz * v3.config.a;
+vx_f(1) = t_vx - est_wz * v3.config.track_width / 2;
+vx_f(2) = t_vx + est_wz * v3.config.track_width / 2;
+delta_0slip = mean(atan2(vy_f, vx_f));
+vx_wheelframe = vx_f .* cos(delta_0slip) + vy_f .* sin(delta_0slip);
+w_wheel_front = vx_wheelframe / v3.config.r_wheel;
 
-%%
-f2 = matlabFunction(lhs(eqn_sub([1 4 5])));
+% create matlab function
+f2 = matlabFunction(lhs(eqn_vsub), 'Vars',{'steering_angle','w_wheel1','w_wheel2','w_yaw','w_wheel_rear'});
+
+% solve
+f2v = @(x) (f2(x(1), x(2), x(3), x(4), x(5)));
 w_r_kinematic = t_vx / v3.config.r_wheel;
-if 1
-    options = optimoptions("fsolve", 'MaxFunctionEvaluations', 1e3);
-    f2b = @(x) f2(x(1), x(2), x(3));
-    [soln, val] = fsolve(f2b, [t_vx, t_vy, w_r_kinematic*1.1], options);
-    val = norm(val)
-else
-    f2b = @(x) norm(f2(x(1), x(2), x(3)));
-    [soln, val] = fmincon(f2b, [t_vx, t_vy, w_r_kinematic*1.01]);
-    val = norm(val)
-end
-r_delta = soln(1);
-r_w_yaw = soln(2);
-r_w_wheel_rear = soln(3);
+[soln, val] = fsolve(f2v, [delta_0slip, w_wheel_front, est_wz, w_r_kinematic]+0.01)
 
 %% functions
 function d = toSym(d)
